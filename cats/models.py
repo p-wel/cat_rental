3efb521c86d19d8edc55b0d838e05c881f256f08
p-wel <wojtowiczp97@gmail.com>
2021-12-07 14:26:01 +0100

    Templates added
    Added html templates.
    Updated Cats model.
    Updated Cats views.
    Prepared new urls.

--- cats/models.py
+++ cats/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 
 class Cat(models.Model):
-    # species: house cat, farm cat, feral cat
-    species = models.CharField(max_length=255)
-    # House cat: American curl, Sphinx, Ragamuffin, Siberian cat, Persian Cat
-    # Farm cat: Ship's cat, Barn cat
-    # Feral cat: Bengal tiger, Lion, Cheetah, Jaguar, Leopard
-    breed = models.CharField(max_length=255)
+    species = models.CharField(max_length=50)
+    # species: House Cat, Farm Cat, Feral Cat
+
+    breed = models.CharField(max_length=50)
+    # House cat: American Curl, Sphinx, Ragamuffin, Siberian Cat, Persian Cat
+    # Farm cat: Sheep's Cat, Barn Cat
+    # Feral cat: Bengal Tiger, Lion, Cheetah, Jaguar, Leopard
+
+    description = models.TextField(default=None, blank=True)
+    available = models.BooleanField(default=True)
+    last_rental_date = models.DateTimeField(default=None, blank=True, null=True)
+    last_return_date = models.DateTimeField(default=None, blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id}. {self.breed} ({self.species})"
--- cats/urls.py
+++ cats/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from cats.views import show_cats
+from cats.views import *
 
 urlpatterns = [
-    path('', show_cats),
+    path('cats/1/rented/', cat_rented),
+    # path('cats/<int:cats_id>/rented/', cat_rented),
+    path('cats/1/', cat_details),
+    # path('cats/<int:pk>/', cat_details),
+
+    path('cats/', cats_list),
+    path('', index),
+
 ]
+
+
