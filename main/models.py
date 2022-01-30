5103af1c7fac12d4a4625a5e02131186a1ce2251
p-wel <wojtowiczp97@gmail.com>
2022-01-30 16:55:52 +0100

    Admin update
    main/admin: updated UserProfile

--- main/models.py
+++ main/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     bio = models.TextField
+
+    def __str__(self):
+        return f"{self.user}'s profile"
