b1173a2a0ffb394fcbd1c6b6598486e87882ab4f
p-wel <wojtowiczp97@gmail.com>
2022-04-16 16:34:26 +0200

    Delete all files

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from main.models import UserProfile
-
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ["user"]
-    search_fields = ["user"]
--- main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.user}'s profile"
